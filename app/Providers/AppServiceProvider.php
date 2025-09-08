f4223315e5b201aa3f174c36eb08df026738842a
Gercova <germancotrina17@gmail.com>
2025-09-07 20:29:52 -0500

    Nuevos agregados

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Enterprise;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        setlocale(LC_TIME, 'es_ES.utf8', 'es_ES', 'es');
+        // Usar un View Composer para compartir variables
+        View::composer('layouts.app', function ($view) {
+            $enterprise = Enterprise::where('id', 1)->get();
+            $view->with(['enterprise' => $enterprise]);
+        });
     }
 }
--- resources/views/Layouts/skeleton.blade.php
+++ resources/views/Layouts/skeleton.blade.php
@@ -6,7 +6,9 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('storage/'.$enterprise[0]->logo) }}">
     <title>@yield('title', $enterprise[0]->nombre_comercial)</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -55,63 +57,61 @@
     <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
     <!-- JTable -->
-    <script src="{{ asset('public/jtable/jquery.jtable.js') }}"></script>
-    <script src="{{ asset('public/jtable/jquery.jtable.min.js') }}"></script>
-    <script src="{{ asset('public/jtable/localization/jquery.jtable.es.js') }}"></script>
+    <script src="{{ asset('jtable/jquery.jtable.js') }}"></script>
+    <script src="{{ asset('jtable/jquery.jtable.min.js') }}"></script>
+    <script src="{{ asset('jtable/localization/jquery.jtable.es.js') }}"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('public/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
     <!-- Select2 -->
-    <script src="{{ asset('public/plugins/select2/js/select2.full.min.js') }}"></script>
+    <script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
     <!-- SweetAlert 2 -->
-    <script src="{{ asset('public/sweetalert2/sweetalert2@11.js') }}"></script>
+    <script src="{{ asset('sweetalert2/sweetalert2@11.js') }}"></script>
     <!-- Funciones JS -->
     <script src="{{ asset('js/forms/funciones.js') }}"></script>
 
     <!---->
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-    <meta http-equiv="Pragma" content="no-cache">
-    <meta http-equiv="Expires" content="0">
+    <!---->
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- JQuery UI -->
+    <!--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    
     <link rel="stylesheet" href="{{ asset('jquery-ui/jquery-ui.min.css') }}">
-    <!-- Font Awesome -->
+    
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
-    <!-- DataTables -->
+    
     <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
-    <!-- Theme style -->
+    
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
-    <!-- JTable -->
+    
     <link rel="stylesheet" href="{{ asset('jtable/themes/lightcolor/gray/jtable.min.css') }}">
-    <!-- iCheck for checkboxes and radio inputs -->
+    
     <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <!-- Bootstrap Color Picker -->
+    
     <link rel="stylesheet" href="{{ asset('plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css') }}">
-    <!-- Select2 -->
+    
     <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/slimselect.min.css') }}">
-    <!-- Summernote -->
+    
     <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }}">
-    <!-- Bootstrap Icon -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css') }}">
+    
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css') }}">-->
     <script>
         const API_BASE_URL = "{{ url('/') }}";
         const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
     </script>
     <!-- Hightcharts -->
-    <script src="{{ asset('highcharts/highcharts.js') }}"></script>
+    <!--<script src="{{ asset('highcharts/highcharts.js') }}"></script>
 	<script src="{{ asset('highcharts/exporting.js') }}"></script>
-    <!-- jQuery -->
+    
     <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Bootstrap 4 -->
+    
     <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <!-- JQuery UI -->
+    
     <script src="{{ asset('jquery-ui/jquery-ui.min.js') }}"></script>
     <script src="{{ asset('js/lodash.min.js') }}"></script>
-    <!-- DataTables  & Plugins -->
+    
     <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
@@ -121,26 +121,26 @@
     <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
-    <!-- JTable -->
+    
     <script src="{{ asset('jtable/jquery.jtable.js') }}"></script>
     <script src="{{ asset('jtable/jquery.jtable.min.js') }}"></script>
     <script src="{{ asset('jtable/localization/jquery.jtable.es.js') }}"></script>
-    <!-- AdminLTE App -->
+    
     <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
-    <!-- Select2 -->
+    
     <script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
-    <!-- SlimSelect -->
+    
     <script src="{{ asset('js/slimselect.min.js') }}"></script>
-    <!-- Summernote -->
+    
     <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
-    <!-- SweetAlert 2 -->
+    
     <script src="{{ asset('sweetalert2/sweetalert2@11.js') }}"></script>
-    <!-- Axios -->
+    
     <script src="{{ asset('js/axios.min.js') }}"></script>
-    <!-- Funciones JS -->
+    
     <script src="{{ asset('js/forms/funciones.js') }}"></script>
     <script src="{{ asset('js/forms/modalDetails.js') }}"></script>
-    <script src="{{ asset('js/forms/deleteHandler.js') }}"></script>
+    <script src="{{ asset('js/forms/deleteHandler.js') }}"></script>-->
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
