1a7b4681fb4c42f18706fbb886d7b4ff73f35832
Gercova <germancotrina17@gmail.com>
2025-11-03 18:00:48 -0500

    Columna especialidad_id añadido a la tabla users

---
+++ database/migrations/2025_11_03_175148_add_especialidad_id_in_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('especialidad_id')->nullable()->after('perfil_id');
+            $table->foreign('especialidad_id')->references('id')->on('especialidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
