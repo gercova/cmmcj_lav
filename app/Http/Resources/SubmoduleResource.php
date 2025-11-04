89c0508a7fdf033f8fbc0ce1ca388ae0f0cf933d
Gercova <germancotrina17@gmail.com>
2025-11-03 23:45:31 -0500

    Recurso Submodule agregado

---
+++ app/Http/Resources/SubmoduleResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SubmoduleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'            => $this->id,
+            'module_id'     => new ModuleResource($this->whenLoaded('module')),
+            'nombre'        => $this->nombre,
+            'descripcion'   => $this->descripcion,
+            'ruta'          => $this->ruta,
+            'icono'         => $this->icono,
+            'orden'         => $this->orden
+        ];
+    }
+}
