3ce25906d71a963ae7e23f9f8c6818203eea134e
Gercova <germancotrina17@gmail.com>
2025-12-06 11:06:11 -0500

    refactor: EnterpriseSeeder incluido

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void {
         $this->call([
+            // empresa
+            EnterpriseSeeder::class,
+
             // historias
             DocumentTypeSeeder::class,
             TipoSangreSeeder::class,
